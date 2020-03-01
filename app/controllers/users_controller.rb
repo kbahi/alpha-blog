79655719daeb575d25e28c6cc244d3832f4220de
rails udemy course <karimbahi@gmail.com>
2020-03-01 22:52:17 +0000

    add admin permissions to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
     
-    before_action :set_user, only: [:edit, :update, :show]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :set_user, only: [:edit, :update, :show, :destroy]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+    before_action :require_admin, only: [:destroy]
     
     def show
         @user_articles = @user.articles.paginate(page: params[:page], per_page: 1)
+    end
+    
+    def destroy
+        @user.destroy
+        flash[:danger] = "User and all articles created by have been deleted"
+        redirect_to users_path
+        
     end
     
     def index
@@ -47,8 +55,14 @@ class UsersController < ApplicationController
         params.require(:user).permit(:username, :email, :password)
     end
     def require_same_user
-        if current_user != @user 
+        if current_user != @user && !current_user.admin?
             flash[:danger] = "You can only edit your own profile"
+            redirect_to root_path
+        end
+    end
+    def require_admin
+        if logged_in? && !current_user.admin?
+            flash[:danger] = "only admin users can perform that action"
             redirect_to root_path
         end
     end
